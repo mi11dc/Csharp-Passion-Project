62c0fa7c53f2ebd6495b5ee7eb420a4714615c8f
mi11dc <mi1996.dhaval@gmail.com>
2023-06-09 22:08:53 -0400

    Initial Commit

---
+++ Csharp-Passion-Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Csharp_Passion_Project.MvcApplication" Language="C#" %>
---
+++ Csharp-Passion-Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Csharp_Passion_Project.Startup))]
+namespace Csharp_Passion_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
